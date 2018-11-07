503c1d2f712f9fb94531c1e1ecea002278d1c00e
1812120133l <1812120133@qq.com>
2018-11-07 21:23:21 +0800

    修改

--- app/Http/Controllers/Order/OrderController.php
+++ app/Http/Controllers/Order/OrderController.php
@@ -19,12 +19,9 @@ class OrderController extends Controller
     //
     public function addorder(Request $request){
       // return $total;
-
        // DB::begiTransaction();
         DB::beginTransaction();
         try {
-
-
             $address_id=$request->address_id;
             $user_id=Auth::user()->id;
             $goods_id=DB::select("select goods_id from carts where user_id = ? ORDER BY goods_id DESC",[$user_id]);
@@ -86,7 +83,9 @@ class OrderController extends Controller
             if($order && $order2){
                 DB::commit();
                 DB::table("carts")->where('user_id',$user_id)->delete();
-
+                $tel=Auth::user()->tel;
+                $str=111;
+                $this->note($tel,$str);
                return  ["status"=> "true", "message"=> "添加成功","order_id"=>$order->id];
             }
         } catch (\Exception $e) {
@@ -149,10 +148,7 @@ class OrderController extends Controller
     }
 
     public function order(){
-       // $id=$_GET['id'];
-        $id=31;
-        //$user_id=3;
-        // return $user_id;
+        $id=$_GET['id'];
         $order=Order::where('id',$id)->first();
         $shop=Shop::where('id',$order->shop_id)->first();
             // return $order->shop_id;
@@ -187,5 +183,59 @@ class OrderController extends Controller
 
 
         return $data;
+    }
+
+    public function note($tel,$str){
+        $params = array ();
+
+        // *** 需用户填写部分 ***
+        // fixme 必填：是否启用https
+        $security = false;
+
+        // fixme 必填: 请参阅 https://ak-console.aliyun.com/ 取得您的AK信息
+        $accessKeyId = "LTAIj3HeUkqPbIJr";
+        $accessKeySecret = "F6rD2vmIoUSWjByRgIaibhgahKWPu3";
+
+        // fixme 必填: 短信接收号码
+        $params["PhoneNumbers"] = $tel;
+
+        // fixme 必填: 短信签名，应严格按"签名名称"填写，请参考: https://dysms.console.aliyun.com/dysms.htm#/develop/sign
+        $params["SignName"] = "苏木情深";
+
+        // fixme 必填: 短信模板Code，应严格按"模板CODE"填写, 请参考: https://dysms.console.aliyun.com/dysms.htm#/develop/template
+        $params["TemplateCode"] = "SMS_150172411";
+
+        // fixme 可选: 设置模板参数, 假如模板中存在变量需要替换则为必填项
+        $params['TemplateParam'] = Array (
+            "product" => $str,
+        );
+
+        // fixme 可选: 设置发送短信流水号
+        $params['OutId'] = "12345";
+
+        // fixme 可选: 上行短信扩展码, 扩展码字段控制在7位或以下，无特殊需求用户请忽略此字段
+        $params['SmsUpExtendCode'] = "1234567";
+
+
+        // *** 需用户填写部分结束, 以下代码若无必要无需更改 ***
+        if(!empty($params["TemplateParam"]) && is_array($params["TemplateParam"])) {
+            $params["TemplateParam"] = json_encode($params["TemplateParam"], JSON_UNESCAPED_UNICODE);
+        }
+
+        // 初始化SignatureHelper实例用于设置参数，签名以及发送请求
+        $helper = new \App\Models\SignatureHelper();
+
+        // 此处可能会抛出异常，注意catch
+        $content = $helper->request(
+            $accessKeyId,
+            $accessKeySecret,
+            "dysmsapi.aliyuncs.com",
+            array_merge($params, array(
+                "RegionId" => "cn-hangzhou",
+                "Action" => "SendSms",
+                "Version" => "2017-05-25",
+            )),
+            $security
+        );
     }
 }
